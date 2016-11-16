ca9bd176d6034d00a671fc318338c8ab3b032884
吳柏宗 <centerseed@hotmail.com>
2016-11-16 14:19:11 +0800

    Resize location icon

--- app/src/main/java/com/moana/carsharing/utils/IconUtils.java
+++ app/src/main/java/com/moana/carsharing/utils/IconUtils.java
@@ -11,10 +11,12 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 
 public class IconUtils {
     public static BitmapDescriptor getMarkerIconFromDrawable(Drawable drawable) {
+        int w = (int) (drawable.getIntrinsicWidth() * 1.4);
+        int h = (int) (drawable.getIntrinsicHeight() * 1.4);
         Canvas canvas = new Canvas();
-        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
         canvas.setBitmap(bitmap);
-        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+        drawable.setBounds(0, 0, w, h);
         drawable.draw(canvas);
         BitmapDescriptor descriptor = BitmapDescriptorFactory.fromBitmap(bitmap);
 
